a328eed92a483bf16d07560de729672a1aa001ab
Veron Ananda Syaputra <104264819+Nanoeo@users.noreply.github.com>
2025-05-23 20:12:34 +0700

    Create #2-Userinput.cpp

---
+++ #2-Userinput.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int brave;
+    cout << "your age? : ";
+    cin >> brave;
+    cout << "your age is:" << brave << endl ;
+
+    string makanan;
+    cout << "Makanan apa yang kamu makan hari ini?"  << endl ;
+    cin >> makanan ;
+    cout << makanan << endl ;
+
+    int x,y ;
+    cout << "Masukan nilai x" << endl ;
+    cin >> x ;
+    cout << "Masukan nilai y" << endl ;
+    cin >> y ;
+
+    int sum=x+y;
+    cout << "Hasilnya adalah:" << sum << endl;
+
+
+    return 0;
+}
